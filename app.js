4b98a1be9ef3be1cbca06144ac0e4bf14986cb09
Robert Russo <rrusso44@gmail.com>
2016-02-29 20:35:57 -0500

    Favicon & Meta Tags

--- app.js
+++ app.js
@@ -14,14 +14,15 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// Tools
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes
 app.use('/', routes);
 app.use('/users', users);
 app.use('/modules', express.static(__dirname + '/node_modules/'));
