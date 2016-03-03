06c6f3dd39d3a3727d6b9f2b19785977286ec826
PeterRyder <peter.w.ryder@gmail.com>
2016-03-02 20:38:30 -0500

    added references file

---
+++ references.js
@@ -0,0 +1,3 @@
+module.exports = {
+	API_KEY : "x0Ek3fBDERrmYdHdKhNZQmeH3L6JY17hYd1aYUCj"
+}
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,21 @@
 var express = require('express');
 var request = require('request');
+var references = require('../references')
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	request('http://api.nal.usda.gov/ndb/search/?format=json&q=oreos&sort=n&max=25&offset=0&api_key=x0Ek3fBDERrmYdHdKhNZQmeH3L6JY17hYd1aYUCj', function (error, response, body) {
+	request('http://api.nal.usda.gov/ndb/search/?format=json&q=oreos&sort=n&max=25&offset=0&api_key=' + references.API_KEY, function (error, response, body) {
 	    if (!error && response.statusCode == 200) {
 	    	var data = JSON.parse(body);
 	    	console.log(data["list"]["q"]);
+	    	var oreos = data["list"]["q"];
 	        res.render('index', { 
 			    title: 'Gro Gro',
 			    jquery: '/modules/jquery/dist/jquery.min.js',
-			    scripts: ['javascript/activenav.js', 'javascript/getfood.js'] 
+			    scripts: ['javascript/activenav.js', 'javascript/getfood.js'],
+			    oreos: oreos
 			});
 	     }
 	});
