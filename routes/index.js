6ed5de673888e51b5dd60793af2f7c93ba6c6221
PeterRyder <peter.w.ryder@gmail.com>
2016-03-02 21:35:25 -0500

    Begun implementing angular

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,34 @@
 var express = require('express');
 var request = require('request');
-var references = require('../references')
 
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	request('http://api.nal.usda.gov/ndb/search/?format=json&q=oreos&sort=n&max=25&offset=0&api_key=' + references.API_KEY, function (error, response, body) {
-	    if (!error && response.statusCode == 200) {
-	    	var data = JSON.parse(body);
-	    	console.log(data["list"]["q"]);
-	    	var oreos = data["list"]["q"];
-	        res.render('index', { 
+
+  var app = angular.module("GroGro", []);
+
+  /* using the controller */
+  app.controller('Controller', function($scope, $http) {
+    $scope.search = function() {
+      request('http://api.nal.usda.gov/ndb/search/?format=json&q=' + $scope.input.text + '&sort=n&max=25&offset=0&api_key=' + references.API_KEY, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          var data = JSON.parse(body);
+          console.log(data);
+          //$scope.test = data[""][""]  
+        }
+      });
+    };
+  });
+
+
+  
+	res.render('index', { 
 			    title: 'Gro Gro',
-			    jquery: '/modules/jquery/dist/jquery.min.js',
-			    scripts: ['javascript/activenav.js', 'javascript/getfood.js'],
-			    oreos: oreos
+			    scripts: ['javascript/activenav.js'],
 			});
-	     }
-	});
 });
+
+
 
 module.exports = router;
