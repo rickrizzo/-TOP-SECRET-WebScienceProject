65dc9d0b9e63aa84e49438ce0fcb49c24724060a
PeterRyder <peter.w.ryder@gmail.com>
2016-03-02 20:02:03 -0500

    Began adding api

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Gro Gro' });
+
+	request('http://api.nal.usda.gov/ndb/search/?format=json&q=oreos&sort=n&max=25&offset=0&api_key=x0Ek3fBDERrmYdHdKhNZQmeH3L6JY17hYd1aYUCj', function (error, response, body) {
+	    if (!error && response.statusCode == 200) {
+	    	var data = JSON.parse(body)
+	    	console.log(data["list"]["q"]);
+	        res.render('index', { title: 'Gro Gro', request: data });
+	     }
+	});
 });
 
 module.exports = router;
