4582c6ba9c32744e82e10899e9e0b633084deeb3
Sarah Abrahamson <abrahs3@rpi.edu>
2016-04-27 20:26:46 -0400

    Updated file paths

--- public/javascript/controllers/loginCtrl.js
+++ public/javascript/controllers/loginCtrl.js
@@ -5,8 +5,7 @@ app.controller('loginCtrl', function($scope, $routeParams, $http) {
 
   $scope.facebook = function(){
   	$http.get("/fb_login/auth/facebook").then(function(response) {
-  		document.getElementById("response").innerHTML = response.data;
-  		document.getElementById("login_form").action = "https://www.facebook.com/login.php?login_attempt=1&amp;next=https%3A%2F%2Fwww.facebook.com%2Fv2.5%2Fdialog%2Foauth%3Fredirect_uri%3Dhttp%253A%252F%252Flocalhost%253A3000%252Fauth%252Ffacebook%252Fcallback%26scope%3Demail%26response_type%3Dcode%26client_id%3D176656516048298%26ret%3Dlogin&amp;lwv=100"
+  		window.open("https://www.facebook.com/login.php?login_attempt=1&amp;next=https%3A%2F%2Fwww.facebook.com%2Fv2.5%2Fdialog%2Foauth%3Fredirect_uri%3Dhttp%253A%252F%252Flocalhost%253A3000%252Fauth%252Ffacebook%252Fcallback%26scope%3Demail%26response_type%3Dcode%26client_id%3D176656516048298%26ret%3Dlogin&amp;lwv=100");
   	});
   };
 });
--- routes/login.js
+++ routes/login.js
@@ -3,13 +3,13 @@ var router = express.Router();
 var request = require('request');
 var passport = require('passport');
 var FacebookStrategy = require('passport-facebook').Strategy;
-var loginCtrl = require('../controller/loginCtrl');
+var loginCtrl = require('../controllers/loginCtrl');
 
 
 passport.use(new FacebookStrategy({
     clientID: '176656516048298',
     clientSecret: 'ce8c2e497fe9da446b6ddfd284dcb26e',
-    callbackURL: "http://localhost:3000/auth/facebook/callback"
+    callbackURL: "http://localhost:3000/fb_login/auth/facebook/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
     loginCtrl.create(profile, 200);
@@ -23,5 +23,8 @@ router.get('/auth/facebook/callback', passport.authenticate('facebook', { failur
     res.redirect('/');
   }
 );
+
+
+
 
 module.exports = router;
