e55bfc62a97050d925fcf70f1adba80e4b48d003
Sarah Abrahamson <abrahs3@rpi.edu>
2016-05-02 17:24:29 -0400

    Changed callback location for facebook

--- routes/login.js
+++ routes/login.js
@@ -13,7 +13,6 @@ router.use(passport.session());
 passport.use(new FacebookStrategy({
     clientID: '176656516048298',
     clientSecret: 'ce8c2e497fe9da446b6ddfd284dcb26e',
-    callbackURL: "http://localhost:3000/fb_login/auth/facebook/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
     userModel.findOne({'fb_id':profile.id},function(err, found){
