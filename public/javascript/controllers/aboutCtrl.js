ec15e8fab80d96b6929b60891b6c02a18c69be7a
Jessie Sodolo <sodolj@rpi.edu>
2016-05-01 22:16:26 -0400

    Meal Log page
    Edited the former About page to create meal.

--- public/javascript/controllers/aboutCtrl.js
+++ public/javascript/controllers/aboutCtrl.js
@@ -2,11 +2,19 @@
 app.controller('aboutCtrl', function($scope, $routeParams, listService) {
   $scope.name = 'aboutCtrl';
   $scope.params = $routeParams;
+  $scope.mealinfo = [];
 
   // Get Latest Grocery List
   $scope.$watch(function() { return listService.getEntries(); }, function(newValue, oldValue) {
     if (newValue != null) {
-      console.log(newValue);
+       var obj = newValue;
+    	//console.log(newValue);
+   		for (var key in obj) {
+		  if (obj.hasOwnProperty(key)) {
+		    var val = obj[key];
+		    $scope.mealinfo.push(val);
+		  }
+		}
     }
   }, true);
 
