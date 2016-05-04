4945478dbdc75fb2938a4e78a0bffb8287f381ae
Jessie Sodolo <sodolj@rpi.edu>
2016-05-04 16:59:18 -0400

    -- Testing about 6

--- routes/api.js
+++ routes/api.js
@@ -90,8 +90,9 @@ router.get('/get_list', function(req, res, next) {
   console.log(req.cookies.user);
   var hold = listCtrl.findOrCreate({name: "TestList", user_id: req.cookies.user});
   for(val in hold){
-    res.send(val);
+    //res.send(val);
   }
+  res.send({this: "test", please: "work"});
   //console.log(hold);
   
 });
