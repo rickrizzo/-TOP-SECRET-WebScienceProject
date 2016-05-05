95833bfecc72733effc865847986b03f7fadbc76
Robert Russo <rrusso44@gmail.com>
2016-05-05 13:46:21 -0400

    Login Controller Changes

--- public/javascript/controllers/loginCtrl.js
+++ public/javascript/controllers/loginCtrl.js
@@ -2,13 +2,22 @@
 app.controller('loginCtrl', function($scope, $routeParams, $http, $window) {
   $scope.name = 'loginCtrl';
   $scope.params = $routeParams;
+  $scope.loggedin = false;
+  $scope.heading = 'Login';
 
   $scope.facebook = function(){
   	$window.location = 'http://' + $window.location.host + '/fb_login/auth/facebook';
-  	$http.get('/fb_login/isloggedin').then(function(res){
-  		if(res){
-  			document.getElementById('login').value = res.data[0].name;
-  		}
-  	});
   };
+  console.log('hello?')
+
+  $http.get('/fb_login/isloggedin').then(function(res){
+    console.log("MAKING REQUEST");
+    if(res){
+      $scope.loggedin = true;
+      console.log(document.getElementById('login').value);
+    } else {
+      console.log("nothing");
+    }
+  });
+
 });
