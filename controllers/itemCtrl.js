521192a3cfaec32b4cfd414e1cf20b5f97298071
Robert Russo <rrusso44@gmail.com>
2016-05-05 13:14:48 -0400

    Crash Fix

--- controllers/itemCtrl.js
+++ controllers/itemCtrl.js
@@ -8,7 +8,7 @@ module.exports = {
 			} else{
 				if(found){
 					if(res) return res.send(JSON.stringify(found));
-				}else{
+				}else if(req.nutrition) {
 					var item = new itemModel({
 						api_id: req.api_id,
 						name: req.nutrition.name,
