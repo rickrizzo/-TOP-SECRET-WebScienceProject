5271dd94dda40143eebc5d9fbf7d263f88320168
Sarah Abrahamson <abrahs3@rpi.edu>
2016-05-05 14:03:11 -0400

    Toggles between Logout/Login with Facebook buttons

--- public/javascript/controllers/loginCtrl.js
+++ public/javascript/controllers/loginCtrl.js
@@ -4,9 +4,9 @@ app.controller('loginCtrl', function($scope, $routeParams, $http, $window) {
   $scope.params = $routeParams;
   $scope.loggedin = false;
   $scope.heading = 'Login';
-
   $scope.facebook = function(){
   	$window.location = 'http://' + $window.location.host + '/fb_login/auth/facebook';
+    $scope.out = true;
   };
 
   $http.get('/fb_login/isloggedin').then(function(res){
@@ -18,5 +18,12 @@ app.controller('loginCtrl', function($scope, $routeParams, $http, $window) {
       console.log("nothing");
     }
   });
+
+  $scope.logout = function(){
+    $http.get('/fb_login/logout').then(function(res){
+      $scope.heading = 'Login';
+      $scope.loggedin = false;
+    });
+  };
 
 });
