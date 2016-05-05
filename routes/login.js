b03000bd4b47971a931559db3da91b71d690c2f7
Rob Russo <rrusso44@gmail.com>
2016-05-05 12:19:14 -0400

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -13,8 +13,8 @@ router.use(passport.session());
 passport.use(new FacebookStrategy({
     clientID: '176656516048298',
     clientSecret: 'ce8c2e497fe9da446b6ddfd284dcb26e',
-    //callbackURL: "https://grogro.herokuapp.com/fb_login/auth/facebook/callback"
-    callbackURL: "http://localhost:3000/fb_login/auth/facebook/callback"
+    callbackURL: "https://grogro.herokuapp.com/fb_login/auth/facebook/callback"
+    //callbackURL: "http://localhost:3000/fb_login/auth/facebook/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
     userModel.findOne({'fb_id':profile.id},function(err, found){
